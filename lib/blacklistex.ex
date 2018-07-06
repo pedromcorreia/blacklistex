478a3df7ca4638480affed5b1c66046733d85d90
pedmcor <pedmcor@gmail.com>
2018-07-05 22:45:19 -0300

    New project

---
+++ lib/blacklistex.ex
@@ -0,0 +1,18 @@
+defmodule Blacklistex do
+  @moduledoc """
+  Documentation for Blacklistex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Blacklistex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/blacklistex_test.exs
@@ -0,0 +1,8 @@
+defmodule BlacklistexTest do
+  use ExUnit.Case
+  doctest Blacklistex
+
+  test "greets the world" do
+    assert Blacklistex.hello() == :world
+  end
+end
